f163bea13e80b0e8062ac62ef0535e87019d37d2
Albie <albievanags@gmail.com>
2025-11-17 13:50:22 +0000

    implemented Vector2 class (reused from previous project)

---
+++ src/utils/Vector2.ts
@@ -0,0 +1,81 @@
+export class Vector2 {
+  protected x: number;
+  protected y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public setX(x: number) {
+    this.x = x;
+  }
+  public setY(y: number) {
+    this.y = y;
+  }
+
+  public getX(): number {
+    return this.x;
+  }
+  public getY(): number {
+    return this.y;
+  }
+
+  public add(a: number): Vector2;
+  public add(a: Vector2): Vector2;
+
+  public add(a: number | Vector2): Vector2 {
+    // for scalar addition
+    if (typeof(a) === "number") {
+      return new Vector2(this.x + a, this.y + a);
+    }
+    //for vector addition
+    else {
+      return new Vector2(this.x + a.getX(), this.y + a.getY());
+    }
+  }
+
+  public subtract(a: number): Vector2;
+  public subtract(a: Vector2): Vector2;
+
+  public subtract(a: number | Vector2): Vector2 {
+    // for scalar subtractition
+    if (typeof(a) === "number") {
+      return new Vector2(this.x - a, this.y - a);
+    }
+    //for vector subtractition
+    else {
+      return new Vector2(this.x - a.getX(), this.y - a.getY());
+    }
+  }
+
+  public copy() {
+    return new Vector2(this.x, this.y);
+  }
+
+  public incrementXBy(x: number) {
+    this.x += x;
+  }
+
+  public incrementYBy(y: number) {
+    this.y += y;
+  }
+
+  public equals(vector: Vector2) {
+    return (
+      this.x == vector.getX() &&
+      this.y == vector.getY()
+    )
+  }
+
+  public applyFunction(func: (n: number) => number) {
+    this.setX(func(this.x));
+    this.setY(func(this.y));
+
+    return this;
+  }
+
+  public mult(num: number) {
+    return new Vector2(this.x * num, this.y * num);
+  }
+}
