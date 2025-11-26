bca807f1d33785e478796b9ca131598e71b84b4b
Albie <albievanags@gmail.com>
2025-11-26 14:19:30 +0000

    commented render manager stuff

--- src/rendering/RenderManager.ts
+++ src/rendering/RenderManager.ts
@@ -20,43 +20,55 @@ export class RenderManager {
   public requestRender(id: string, payload: RenderPayload) {
     // is there an existing render to be completed for this renderable this frame
     const existing = this.pending.get(id);
-    let normalisedPayload: RenderPayload;
 
     // if there is an existing render, merge it with the newest one
-    normalisedPayload = existing
+    const normalisedPayload = existing
       ? this.mergePayloads(existing, payload)
       : payload;
-    
     this.pending.set(id, normalisedPayload);
 
     this.requestFlush();
   }
 
+  /**
+   * Flush on the next animation frame.
+   * Can only happen a max of once per frame, no matter how many times called
+   */
   private requestFlush() {
     if (!this.scheduled) {
       this.scheduled = true;
-      requestAnimationFrame(() => this.flush());
+      requestAnimationFrame(() => {
+        this.flush();
+        this.scheduled = false
+      });
     }
   }
 
   private mergePayloads(original: RenderPayload, toAdd: RenderPayload): RenderPayload {
     const newPayload: RenderPayload = {};
 
+    // toAdd always comes after the original.
+    // It doesn't make sense to run an intial render after other renders have been requested,
+    // so throw an error
     if (toAdd.initial) {
       throw new Error("cannot run an initial render after other renders");
     }
-    // needs updating for movement after zoom
+
+    // needs updating for movement after zoom maybe?
+    // might not be humanly possible though, and if it is it won't make a big difference
     if (toAdd.movement) {
+      // add the movements together
       newPayload.movement = original.movement
         ? original.movement.add(toAdd.movement)
         : toAdd.movement
     }
+
     if (toAdd.zoom) {
       newPayload.zoom = {
-        delta: original.zoom 
+        delta: original.zoom          // add zooms
           ? original.zoom.delta + toAdd.zoom.delta
           : toAdd.zoom.delta,
-        mousePos: toAdd.zoom.mousePos
+        mousePos: toAdd.zoom.mousePos // mouse pos is just the latest. will never be different from the first by more than a few frames
       }
     }
 
@@ -64,6 +76,7 @@ export class RenderManager {
   }
 
   private flush() {
+    // iterate through pending render payloads
     for (let [id, payload] of this.pending) {
       const renderable = this.renderablesById.get(id);
       if (!renderable) {
@@ -74,7 +87,6 @@ export class RenderManager {
     }
 
     this.pending.clear();
-    this.scheduled = false;
   }
 
   public addRenderable(renderable: Renderable) {
